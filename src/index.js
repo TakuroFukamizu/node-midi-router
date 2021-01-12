848329390a4d37c8e534430e8898c286dbd54630
Takuro Fukamizu <takuro.f.1987@gmail.com>
2021-01-13 06:36:56 +0900

    auto refresh devices

--- src/index.js
+++ src/index.js
@@ -6,17 +6,34 @@ const emitter = new EventEmitter();
 let inputs = [];
 let outputs = [];
 
-const kinds = ['noteon', 'noteoff'];
+const kinds = [
+    'noteon', 'noteoff',
+    'poly aftertouch', 'cc', 'program', 'channel aftertouch', 'pitch', 'position', 'mtc', 'select',
+    'clock', 'start', 'continue', 'stop', 'activesense', 'reset', 'sysex'
+];
 
 function getDevices() { 
     const inputNames = easymidi.getInputs();
     const outputNames = easymidi.getOutputs();
-    // FIXME: 差分チェック
+
+    // 差分チェック
+    const newValue = inputNames.join('') + outputNames.join('');
+    const oldValue = inputs.map(v => v.name).join('') + outputs.map(v => v.name).join('');
+    if (newValue == oldValue) { 
+        return; // 更新不要
+    }
+
+    // 使用中のデバイスはcloseする
+    // MidiOutWinMM::openPort: error creating Windows MM MIDI output port.
+    outputs.forEach((v) => v.close());
+    inputs.forEach((v) => v.close());
+
+    // デバイス更新
     inputs = inputNames.map((v) => new easymidi.Input(v));
     outputs = outputNames.map((v) => new easymidi.Output(v));
-}
+    console.debug('update devices', inputs.map((v) => v.name), outputs.map((v) => v.name));
 
-function setupListeners() { 
+    // Input->Outputとなるようにeventbusをセット
     for (const kind of kinds) {
         for (const input of inputs) { 
             // console.log(input.eventNames());
@@ -26,6 +43,7 @@ function setupListeners() {
                 console.log(input.name, kind, params);
             }); 
         }
+        emitter.removeAllListeners(kind);
         emitter.on(kind, (params) => { 
             for (const output of outputs) {
                 output.send(kind, params);
@@ -36,7 +54,13 @@ function setupListeners() {
 
 //----
 
+
 getDevices();
 console.debug(inputs.map((v) => v.name), outputs.map((v) => v.name));
 
-setupListeners();
+// ---
+
+setInterval(() => { 
+    getDevices();
+}, 1000);
+
